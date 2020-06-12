ac17b939ac089668296e61a5174ddd9fbc4b5361
Aparna M <aparnamanoj98@gmail.com>
2020-06-13 03:34:03 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -4,50 +4,8 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('../../config.json')
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        validate: value => {
-            if (!validator.isEmail(value)) {
-                throw new Error({error: 'Invalid Email address'})
-            }
-        }
-    },
-    mobile: {
-        type: String,
-        required: true,
-        minLength: 10,
-        maxLength: 10
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
-})
-
-userSchema.pre('save', async function (next) {
-    // Hash the password before saving the user model
-    const user = this
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
-    next()
-})
+const userSchema = mongoose.Schema({first_name: String, last_name: String, mobile: String, email: String, password: String, 
+    tokens:[{token: String}], otp: String, ethAcct: String, otpStartTime: String }, { collection : 'voterList' });
 
 userSchema.methods.generateAuthToken = async function() {
     // Generate an auth token for the user
@@ -60,12 +18,13 @@ userSchema.methods.generateAuthToken = async function() {
 
 userSchema.statics.findByCredentials = async (email, password) => {
     // Search for a user by email and password.
-    const user = await User.findOne({ email} )
+    const user = await User.findOne({ email} );
     if (!user) {
         throw new Error({ error: 'Invalid login credentials' })
     }
+    /****USE HASH****/
     //const isPasswordMatch = await bcrypt.compare(password, user.password)
-    const isPasswordMatch = password == user.password
+    const isPasswordMatch = password == user.password;
     if (!isPasswordMatch) {
         throw new Error({ error: 'Invalid login credentials' })
     }
