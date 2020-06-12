b30c328bad9bf88c1c4fda7dcc1c3c56a27e5a2c
Aparna M <aparnamanoj98@gmail.com>
2020-06-13 03:33:33 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/newDB",{ useNewUrlParser: true , useUnifiedTopology: true }, (error) => {
-	if(!error)
-		console.log("Success");
-	else
-		console.log("Error connecting to testDB");
-})
