a5f6aabc375961087bed094ef0a572635feb4ec5
Aparna M <aparnamanoj98@gmail.com>
2020-06-16 01:12:19 +0530

    updated to access from config file

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const config = require('../../config.json')
 
 const auth = async(req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', '')
-    const data = jwt.verify(token, process.env.JWT_KEY)
+    const data = jwt.verify(token, config.JWT_KEY)
     try {
         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
         if (!user) {
@@ -13,7 +14,7 @@ const auth = async(req, res, next) => {
         req.token = token
         next()
     } catch (error) {
-        res.status(401).send({ error: 'Not authorized to access this resource' })
+        res.status(401).send({ error: 'Invalid token: Not authorized to access this resource' })
     }
 
 }
